4ffc8be4e3f67fe4b25554ec41b0e0ffd67c7ff0
nonknee <97688130+nonknee@users.noreply.github.com>
2023-03-06 13:20:47 -0500

    Added placeholder enemies, an object to spawn them, and created prefabs for them
    To use the enemies, place down their prefabs and create empty game objects with a transform parameters to be the points the enemy travels to. As of this commit, the enemies just delete the player on contact.

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public Transform[] points;
+    int current;
+    public float speed;
+
+    void Start()
+    {
+        
+        current = 0;
+    }
+
+
+    void Update()
+    {
+        if (transform.position != points[current].position)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, points[current].position, speed * Time.deltaTime);
+        }
+        else
+        {
+            current = (current + 1) % points.Length;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
