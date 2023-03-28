1714943e9c2de701e4706d3c643d34b10837dd66
Stephanie7272 <stephanie.pileggi@knights.ucf.edu>
2023-03-27 22:59:03 -0400

    Added door function to V3 door prefab

---
+++ Assets/Scripts/DoorTrigger.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    [SerializeField] private Animator doorV3 = null;
+
+    [SerializeField] private bool openTrigger = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (openTrigger)
+            {
+                doorV3.Play("DoorOpenV3", 0, 0.0f);
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
