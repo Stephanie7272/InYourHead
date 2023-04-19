d98a3c81ab4fe79fd8391e0958a32e8df1f75d42
Stephanie7272 <stephanie.pileggi@knights.ucf.edu>
2023-04-19 09:44:38 -0400

    Added Puzzle Two Pick Up and Puzzle Documents

---
+++ Assets/Scripts/PuzzleTwoPickUp.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleTwoPickUp : MonoBehaviour
+{
+  private bool isPickedUp = false;
+  public GameObject CanvasPuzzleTwo;
+
+  void Start()
+  {
+        CanvasPuzzleTwo.SetActive(false);
+        isPickedUp = false;
+  }
+  void OnMouseDown()
+  {
+    if (isPickedUp)
+    {
+        CanvasPuzzleTwo.SetActive(false);
+        isPickedUp = false;
+    }
+    else
+    {
+        CanvasPuzzleTwo.SetActive(true);
+        isPickedUp = true;
+    }
+  }
+}
